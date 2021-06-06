2f9f2ee0539898c3d9617245fb50437ae39a2284
jinfu lu <413356854@qq.com>
2021-06-06 16:16:26 +0800

    init

---
+++ auto-generator-plus/src/main/java/com/example/autogeneratorplus/AutoGeneratorPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.autogeneratorplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGeneratorPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoGeneratorPlusApplication.class, args);
+	}
+
+}
---
+++ auto-generator-plus/src/test/java/com/example/autogeneratorplus/AutoGeneratorPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autogeneratorplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoGeneratorPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
