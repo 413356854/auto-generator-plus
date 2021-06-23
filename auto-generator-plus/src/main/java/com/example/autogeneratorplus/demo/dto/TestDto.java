630af7f2d295a7bc9459bbddf24d9f20471d32f4
ljf <413356854@qq.com>
2021-06-24 03:03:13 +0800

    dto  createFile

---
+++ auto-generator-plus/src/main/java/com/example/autogeneratorplus/demo/dto/TestDto.java
@@ -0,0 +1,13 @@
+package com.example.autogeneratorplus.demo.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class TestDto {
+
+    @ApiModelProperty("交易HASH")
+    private String name;
+
+
+}
--- auto-generator-plus/src/main/java/com/example/autogeneratorplus/generator/InterfaceGenerator.java
+++ auto-generator-plus/src/main/java/com/example/autogeneratorplus/generator/InterfaceGenerator.java
@@ -1,7 +1,10 @@
 package com.example.autogeneratorplus.generator;
 
-import com.example.autogeneratorplus.demo.entity.User;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.example.autogeneratorplus.demo.entity.BaseAccount;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
@@ -10,21 +13,51 @@ import java.lang.reflect.Modifier;
  */
 public class InterfaceGenerator {
 
-    public static void createDto(){
-        
+    public static void main(String[] args) {
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.example.autogeneratorplus.demo");
+        Class objClass = BaseAccount.class;
+        //接口生成
+        createList(pc,objClass);
     }
 
+    private static void createList(PackageConfig pc,Class objClass){
+        //根目录
+        String projectPath = System.getProperty("user.dir");
+        //模块路径
+        String parentPath = "/src/main/java/"+pc.getParent().replace(".", "/");
+        //dto文件路径
+        String dtoPath = projectPath + parentPath + "/dto";
+        //创建dto文件
+        createFile(dtoPath,objClass.getSimpleName() + "Dto.java");
 
-    public static void main(String[] args) {
-        Class objClass = User.class;
+        System.out.println(dtoPath);
         Field[] fields = objClass.getDeclaredFields();
-//        Field[] fields = objClass.getFields();
         for (Field field : fields) {
             //过滤static修饰
             if (Modifier.isStatic(field.getModifiers())) {
                 continue;
             }
             System.out.println(field.getName());
+            System.out.println(field.getType());
         }
+
+    }
+
+    public static boolean createFile(String path,String fileName){
+        File dir = new File(path);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        File file = new File(path+"/"+fileName);
+        if (!file.exists()) {
+            try {
+                return file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
     }
 }
